5c13544fc743029f94c17e51727e6e3ad037c6d5
Alphabb07 <162568674+Alphabb07@users.noreply.github.com>
2024-12-27 09:34:17 +0100

    Design update - Elements Update
    Creata la griglia dopo diversi tentativi, lo stile della pagina è stato aggiungo (al momento è semplice) nel prossim update ci sarà lo script

--- STATIC/script.js
+++ STATIC/script.js
@@ -1,1 +1,6 @@
 console.log("Javascript è stato caricato correttamente")
+const griglia = document.querySelector('.griglia')
+const iniziogioco = document.querySelector('.iniziogioco')
+const punteggio = document.querySelector('.punteggio')
+let punteggio = 0;
+let temporimasto = 30;
